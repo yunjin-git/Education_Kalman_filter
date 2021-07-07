075bf6665183334bddf7e15a0d6a6a92c43bd972
yunjin-git <75968161+yunjin-git@users.noreply.github.com>
2021-07-07 15:31:52 +0900

    Add files via upload

--- Chap1 Average Filter/Avgfilter.py
+++ Chap1 Average Filter/Avgfilter.py
@@ -5,7 +5,7 @@ import numpy as np
 def Avgfilter (data):
     n=1
     num_of_data = data.shape[0]
-    avg = np.zeros(num_of_data)# Return Array init
+    avg = np.zeros(num_of_data)# Return average data Array init
     while n <= num_of_data:
         avg[n-1]=(n-1)/(n)*avg[n-2]+(1/n)*(data[n-1])
         n=n+1
